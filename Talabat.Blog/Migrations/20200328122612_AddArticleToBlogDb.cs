abe28e5be86c13436894193d9420411731fb40ed
Abdulrahman Alaa <abdelraman.bashir@vodafone.com>
2020-03-28 16:27:15 +0400

    Adding Package Microsoft.EntityFrameworkCore.Tools and run add-migration AddArticleToBlogDb

---
+++ Talabat.Blog/Migrations/20200328122612_AddArticleToBlogDb.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Talabat.Blog.Migrations
+{
+    public partial class AddArticleToBlogDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Article",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Article", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Article");
+        }
+    }
+}
