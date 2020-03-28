f1d0031c0603c97c74d842c713c7bc2151ebd01e
Abdulrahman Alaa <abdelraman.bashir@vodafone.com>
2020-03-28 16:53:58 +0400

    Create Db with update-database and create ArticlesList  Page with simple bootstrap header

---
+++ Talabat.Blog/Pages/Articles/index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Talabat.Blog.Models;
+
+namespace Talabat.Blog.Pages.Articles
+{
+    public class indexModel : PageModel
+    {
+        private  ApplicationDbContext Db { get; set; }
+
+        public IEnumerable<Article> Articles { get; set; }
+        public indexModel(ApplicationDbContext db)
+        {
+            Db = db;
+        }
+        public async Task OnGet()
+        {
+            Articles = await Db.Article.ToListAsync();
+        }
+    }
+}
